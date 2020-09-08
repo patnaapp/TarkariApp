ce3859161a506079972d7bca1143610a66fc1e8e
patnaapp <patnaapp@gmail.com>
2020-09-08 12:08:09 +0530

    updates

--- app/src/main/java/bih/in/tarkariapp/activity/deliveryboy/DeliveryBoy_Home_Activity.java
+++ app/src/main/java/bih/in/tarkariapp/activity/deliveryboy/DeliveryBoy_Home_Activity.java
@@ -2,11 +2,18 @@ package bih.in.tarkariapp.activity.deliveryboy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.View;
 import android.widget.TextView;
 
 import bih.in.tarkariapp.R;
+import bih.in.tarkariapp.activity.PreLoginActivity;
+import bih.in.tarkariapp.activity.thelawala.HomeActivity;
 
 public class DeliveryBoy_Home_Activity extends AppCompatActivity {
 
@@ -34,5 +41,38 @@ public class DeliveryBoy_Home_Activity extends AppCompatActivity {
         tv_username=findViewById(R.id.tv_username);
         tv_regno=findViewById(R.id.tv_regno);
         tv_phone=findViewById(R.id.tv_phone);
+    }
+
+    public void OnClickLogout(View view)
+    {
+        new AlertDialog.Builder(DeliveryBoy_Home_Activity.this)
+                .setTitle("लॉग आउट ??")
+                .setIcon(R.drawable.logo)
+                .setMessage("क्या आप लॉग आउट करना चाहते हैं  \n ")
+                .setCancelable(false)
+                .setPositiveButton("हाँ", new DialogInterface.OnClickListener()
+                {
+                    public void onClick(DialogInterface dialog, int id)
+                    {
+                        confirmLogout();
+                    }
+                })
+                .setNegativeButton("नहीं", null)
+                .show();
+    }
+
+    private void confirmLogout()
+    {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(DeliveryBoy_Home_Activity.this);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putBoolean("username", false);
+        editor.putBoolean("username", false);
+        editor.putString("logintype", "");
+        editor.commit();
+
+        Intent intent = new Intent(this, PreLoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK| Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 }
